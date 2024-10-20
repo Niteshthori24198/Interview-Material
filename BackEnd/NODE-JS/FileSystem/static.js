a25c1d097dc1f62f0072451e4f974810fe14c1f6
Nitesh Kumar <62844049+Niteshthori24198@users.noreply.github.com>
2024-10-20 15:33:55 +0530

    Update static.js

--- BackEnd/NODE-JS/FileSystem/static.js
+++ BackEnd/NODE-JS/FileSystem/static.js
@@ -1,81 +1,48 @@
+let obj = {
+    't': []
+}
+const fs = require('fs');
+const path = require('path');
 
-const fs = require('fs')
+const dirPath = "C:\\Users\\PC\\Desktop\\t";
 
-const path = require('path')
-
-
-function listDirectoryContent(dirPath) {
-
-    try {
-        const files = []
-
-        const folders = []
-
-        // read the content of entire directory;
-
-        const fileAndFolders = fs.readdirSync(dirPath)
+if (fs.existsSync(dirPath)) {
+    readDir(dirPath, obj["t"]);
+} else {
+    console.log("byy")
+    return
+}
 
 
-        // iterate throwgh the readed content and seperate files and folders
 
-        for (let item of fileAndFolders) {
+function readDir(dir, ob) {
+    
+    const fileAndFolders = fs.readdirSync(dir)
+    let file = [];
+    let folders = [];
 
-            // create a full path for the item
-
-            const itempath = path.join(dirPath, item)
-
-            const isFile = fs.statSync(itempath).isFile()
-
-            if (isFile) {
-                files.push(item)
-            } else {
-                folders.push(item)
-            }
-
+    for (let item of fileAndFolders) {
+        const itempath = path.join(dir, item);
+        if (fs.statSync(itempath).isFile()) {
+            file.push(item);
+        } else {
+            folders.push(itempath)
         }
-
-        // display files
-
-        console.log('Files : ')
-        for (let file of files) {
-            console.log(file)
-        }
-
-        // display folders 
-
-        console.log('Folders :')
-        for(let folder of folders){
-            console.log(folder)
-        }
-
-        // iterate through folders
-
-        for (let folder of folders) {
-
-            // create new direcoty path to read
-
-            const folderpath = path.join(dirPath, folder)
-
-            listDirectoryContent(folderpath)
-
-        }
-
-    } catch (error) {
-        console.log(error.message)
     }
 
+    for (let f of file) {
+        ob.push(f)
+    }
+
+    for (let fol of folders) {
+        let sp = fol.split('\\')
+        let pt = sp[sp.length - 1]
+        ob.push({
+            [pt]:[]
+        })
+        readDir(fol, ob[ob.length-1][pt])
+    }
 
 }
 
-
-
-
-const targetDir = 'C:/Users/PC/Desktop/Final-Interview-Preperation/BackEnd/NODE-JS/Basic'
-
-// to check whether the dir exists or not
-
-if (fs.existsSync(targetDir)) {
-    listDirectoryContent(targetDir)
-} else {
-    console.log('Directory does not exists')
-}
+console.log(JSON.stringify(obj))
